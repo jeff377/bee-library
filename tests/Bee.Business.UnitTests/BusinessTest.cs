9633867adb6f60540b9648c5211f48f5167497d0
蔡明志 <jeff377@gmail.com>
2025-04-16 22:34:37 +0800

    ．新增 Bee.Business.UnitTests 測試專案。 ．新增 Bee.Connect.UnitTests 測試專案。

---
+++ tests/Bee.Business.UnitTests/BusinessTest.cs
@@ -0,0 +1,11 @@
+namespace Bee.Business.UnitTests
+{
+    public class BusinessTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/Bee.Connect.UnitTests/ConnectTest.cs
@@ -0,0 +1,11 @@
+namespace Bee.Connect.UnitTests
+{
+    public class ConnectTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
