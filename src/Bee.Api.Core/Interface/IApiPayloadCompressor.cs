7472a51dc4fd07216f78f2313349a6f3ce311e92
蔡明志 <jeff377@gmail.com>
2025-05-18 20:08:27 +0800

    ．新增 IApiPayloadCompressor 介面，API 傳輸層資料壓縮策略介面。 ．新增 IApiPayloadEncryptor 介面，API 傳輸層資料加解密策略介面。

---
+++ src/Bee.Api.Core/Interface/IApiPayloadCompressor.cs
@@ -0,0 +1,29 @@
+﻿namespace Bee.Api.Core
+{
+    /// <summary>
+    /// API 傳輸層資料壓縮策略介面。
+    /// 提供位元組資料的壓縮與還原功能，可用於縮小傳輸體積。
+    /// </summary>
+    public interface IApiPayloadCompressor
+    {
+        /// <summary>
+        /// 壓縮演算法的識別字串，例如 "gzip"、"brotli"。
+        /// </summary>
+        string CompressionMethod { get; }
+
+        /// <summary>
+        /// 將原始位元組資料進行壓縮處理。
+        /// </summary>
+        /// <param name="data">原始位元組資料。</param>
+        /// <returns>壓縮後的位元組資料。</returns>
+        byte[] Compress(byte[] data);
+
+        /// <summary>
+        /// 將壓縮過的位元組資料還原為原始資料。
+        /// </summary>
+        /// <param name="data">壓縮後的位元組資料。</param>
+        /// <returns>解壓縮後的位元組資料。</returns>
+        byte[] Decompress(byte[] data);
+    }
+
+}
---
+++ src/Bee.Api.Core/Interface/IApiPayloadEncryptor.cs
@@ -0,0 +1,29 @@
+﻿namespace Bee.Api.Core
+{
+    /// <summary>
+    /// API 傳輸層資料加解密策略介面。
+    /// 提供位元組資料的加密與解密功能，用以保護傳輸過程中的資料安全。
+    /// </summary>
+    public interface IApiPayloadEncryptor
+    {
+        /// <summary>
+        /// 加密演算法的識別字串，例如 "aes"、"rsa"。
+        /// </summary>
+        string EncryptionMethod { get; }
+
+        /// <summary>
+        /// 將原始位元組資料進行加密處理。
+        /// </summary>
+        /// <param name="data">原始位元組資料。</param>
+        /// <returns>加密後的位元組資料。</returns>
+        byte[] Encrypt(byte[] data);
+
+        /// <summary>
+        /// 將加密過的位元組資料還原為原始資料。
+        /// </summary>
+        /// <param name="data">加密後的位元組資料。</param>
+        /// <returns>解密後的位元組資料。</returns>
+        byte[] Decrypt(byte[] data);
+    }
+
+}
