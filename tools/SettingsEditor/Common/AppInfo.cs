60bf6fef0d7d7d9e71a50839fb653f939cf13ebf
蔡明志 <jeff377@gmail.com>
2025-05-20 17:14:43 +0800

    ．改用 Microsoft.Data.SqlClient 套件存取 SQL Server 資料庫。

--- tools/SettingsEditor/Common/AppInfo.cs
+++ tools/SettingsEditor/Common/AppInfo.cs
@@ -25,7 +25,7 @@ namespace SettingsEditor
                 BackendInfo.BusinessObjectProvider = new Bee.Cache.TBusinessObjectProvider();
                 BackendInfo.SystemObject = new Bee.Business.TSystemObject();
                 // 註冊資料庫提供者
-                DbProviderManager.RegisterProvider(EDatabaseType.SQLServer, System.Data.SqlClient.SqlClientFactory.Instance);
+                DbProviderManager.RegisterProvider(EDatabaseType.SQLServer, Microsoft.Data.SqlClient.SqlClientFactory.Instance);
             }
 
             return true;
