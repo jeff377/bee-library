c9559a13f0c21e78f31283e58efc647354cf922e
蔡明志 <jeff377@gmail.com>
2025-07-26 22:29:30 +0800

    ．新增 Bee.Tests.Shared 專案，共用測試專案。

---
+++ tests/Bee.Tests.Shared/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bee.Tests.Shared
+{
+    /// <summary>
+    /// 測試基底類別，統一掛上全域 Collection 定義。
+    /// </summary>
+    [Collection("Initialize")]
+    public abstract class BaseTest
+    {
+    }
+}
