b05302d5232757ee7b7668e69c2180d5e5caa86d
蔡明志 <jeff377@gmail.com>
2025-09-10 11:09:57 +0800

    ．新增 ITraceDisplayForm 介面，定義可接收追蹤事件的表單介面。 ．新增 FormTraceWriter 類別，將追蹤事件輸出至實作 ITraceDisplayForm 的表單。

---
+++ src/Bee.UI.WinForms/Tracing/FormTraceWriter.cs
@@ -0,0 +1,33 @@
+﻿using Bee.Base;
+
+namespace Bee.UI.WinForms
+{
+    /// <summary>
+    /// 將追蹤事件輸出至實作 <see cref="ITraceDisplayForm"/> 的表單。
+    /// </summary>
+    public class FormTraceWriter : ITraceWriter
+    {
+        private readonly ITraceDisplayForm _form;
+        private readonly SynchronizationContext _uiContext;
+
+        /// <summary>
+        /// 建構函式。
+        /// </summary>
+        /// <param name="form">接收追蹤事件的表單。</param>
+        public FormTraceWriter(ITraceDisplayForm form)
+        {
+            _form = form ?? throw new ArgumentNullException(nameof(form));
+            _uiContext = SynchronizationContext.Current ?? new WindowsFormsSynchronizationContext();
+        }
+
+        /// <summary>
+        /// 寫入一筆追蹤事件。
+        /// </summary>
+        /// <param name="evt">追蹤事件內容。</param>
+        public void Write(TraceEvent evt)
+        {
+            _uiContext.Post(_ => _form.AppendTrace(evt), null);
+        }
+    }
+}
+
---
+++ src/Bee.UI.WinForms/Tracing/ITraceDisplayForm.cs
@@ -0,0 +1,16 @@
+﻿using Bee.Base;
+
+namespace Bee.UI.WinForms
+{
+    /// <summary>
+    /// 定義可接收追蹤事件的表單介面。
+    /// </summary>
+    public interface ITraceDisplayForm
+    {
+        /// <summary>
+        /// 顯示一筆追蹤事件。
+        /// </summary>
+        /// <param name="evt">追蹤事件內容。</param>
+        void AppendTrace(TraceEvent evt);
+    }
+}
