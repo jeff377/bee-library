214b7dc052045c2bba1121fe4a19581c3f461aa2
蔡明志 <jeff377@gmail.com>
2025-10-19 18:05:37 +0800

    新增 FilterNodeCollection 類別並支援批次加入節點

---
+++ src/Bee.Define/Filters/FilterNodeCollection.cs
@@ -0,0 +1,28 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+
+namespace Bee.Define
+{
+    /// <summary>
+    /// 過濾節點集合。
+    /// </summary>
+    [MessagePackObject]
+    [Serializable]
+    public class FilterNodeCollection : MessagePackKeyCollectionBase<FilterNode>
+    {
+        /// <summary>
+        /// 批次加入多個 <see cref="FilterNode"/> 成員。
+        /// </summary>
+        /// <param name="nodes">要加入的節點集合。</param>
+        public void AddRange(IEnumerable<FilterNode> nodes)
+        {
+            if (nodes == null) return;
+            foreach (var node in nodes)
+            {
+                if (node != null)
+                    this.Add(node);
+            }
+        }
+    }
+}
