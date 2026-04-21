079207a2f6856cf70f4c5b56ffb9f9366cf62275
jeff377 <jeff377@gmail.com>
2026-04-21 22:45:16 +0800

    test(sonar-autofix): 故意引入 S2344 + S1116 測試多規則 autofix
    - SerializeState.cs: enum 加上 : int (S2344)
    - ConnectionTestResult.cs: 多餘的 ; (S1116)

--- src/Bee.Base/Serialization/SerializeState.cs
+++ src/Bee.Base/Serialization/SerializeState.cs
@@ -3,7 +3,7 @@ namespace Bee.Base.Serialization
     /// <summary>
     /// Serialization state.
     /// </summary>
-    public enum SerializeState
+    public enum SerializeState : int
     {
         /// <summary>
         /// None.
