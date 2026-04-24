e56ed29d50ae449249e049256260834d8407a8b5
Jeff <jeff377@gmail.com>
2026-04-24 18:15:45 +0800

    test(business): 補 CacheDataSourceProvider 測試覆蓋率 (#24)

---
+++ tests/Bee.Business.UnitTests/CacheDataSourceProviderTests.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel;
+using Bee.Business.Provider;
+using Bee.Tests.Shared;
+
+namespace Bee.Business.UnitTests
+{
+    /// <summary>
+    /// <see cref="CacheDataSourceProvider"/> 測試。
+    /// </summary>
+    [Collection("Initialize")]
+    public class CacheDataSourceProviderTests
+    {
+        [DbFact]
+        [DisplayName("GetSessionUser 傳入不存在的 Token 應回傳 null")]
+        public void GetSessionUser_UnknownToken_ReturnsNull()
+        {
+            var provider = new CacheDataSourceProvider();
+
+            var result = provider.GetSessionUser(Guid.NewGuid());
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        [DisplayName("CacheDataSourceProvider 建構子應正常初始化")]
+        public void Constructor_Default_CreatesInstance()
+        {
+            var exception = Record.Exception(() => new CacheDataSourceProvider());
+
+            Assert.Null(exception);
+        }
+    }
+}
